20f8842240d8bd06bb343f24412008e177cc0e19
Scott Olson <scott@scott-olson.org>
2009-11-28 12:54:28 -0600

    added stdint.h

---
+++ include/stdint.h
@@ -0,0 +1,14 @@
+#ifndef STDINT_H
+#define STDINT_H
+
+typedef unsigned long long uint64_t;
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
+
+typedef signed long long int64_t;
+typedef signed int int32_t;
+typedef signed short int16_t;
+typedef signed char int8_t;
+
+#endif
