4d02a20c9e758d9994b6901235e45f40d4369e94
Scott Olson <scott@scott-olson.org>
2009-11-28 12:45:46 -0600

    added header guard to stdarg.h

--- include/stdarg.h
+++ include/stdarg.h
@@ -1,3 +1,6 @@
+#ifndef STDARG_H
+#define STDARG_H
+
 #ifndef __GNUC__
 #error I want gcc!
 #endif
@@ -7,3 +10,5 @@
 #define va_end(v) __builtin_va_end(v)
 #define va_copy(d,s) __builtin_va_copy(d,s)
 typedef __builtin_va_list va_list;
+
+#endif
