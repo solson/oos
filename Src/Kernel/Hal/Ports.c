872356d509b0cc363c94e85348d4c0d82d41a80f
Scott Olson <scott@scott-olson.org>
2010-02-11 20:19:18 -0600

    Renamed the Ports.c functions.

--- Src/Kernel/Hal/Ports.c
+++ Src/Kernel/Hal/Ports.c
@@ -1,35 +1,35 @@
 #include <stdint.h>
 
-void halOutPortByte(uint16_t port, uint8_t val)
+void outByte(uint16_t port, uint8_t val)
 {
 	__asm volatile("outb %0, %1" : : "a"(val), "d" (port));
 }
 
-uint8_t halInPortByte(uint16_t port)
+uint8_t inByte(uint16_t port)
 {
 	uint8_t val;
 	__asm volatile("inb %1, %0" : "=a" (val) : "d" (port));
 	return val;
 }
 
-void halOutPortWord(uint16_t port, uint16_t val)
+void outWord(uint16_t port, uint16_t val)
 {
 	__asm volatile("outw %0, %1" : : "a"(val), "d" (port));
 }
 
-uint16_t halInPortWord(uint16_t port)
+uint16_t inWord(uint16_t port)
 {
 	uint16_t val;
 	__asm volatile("inw %1, %0" : "=a" (val) : "d" (port));
 	return val;
 }
 
-void halOutPortLong(uint16_t port, uint32_t val)
+void outLong(uint16_t port, uint32_t val)
 {
 	__asm volatile("outl %0, %1" : : "a"(val), "d" (port));
 }
 
-uint32_t halInPortLong(uint16_t port)
+uint32_t inLong(uint16_t port)
 {
 	uint32_t val;
 	__asm volatile("inl %1, %0" : "=a" (val) : "d" (port));
--- Src/Kernel/Hal/Ports.ooc
+++ Src/Kernel/Hal/Ports.ooc
@@ -1,8 +1,8 @@
-outByte: extern(halOutPortByte) proto func (port: UInt16, val: UInt8)
-inByte:  extern(halInPortByte)  proto func (port: UInt16) -> UInt8
+outByte: extern proto func (port: UInt16, val: UInt8)
+inByte:  extern proto func (port: UInt16) -> UInt8
 
-outWord: extern(halOutPortWord) proto func (port: UInt16, val: UInt16)
-inWord:  extern(halInPortWord)  proto func (port: UInt16) -> UInt16
+outWord: extern proto func (port: UInt16, val: UInt16)
+inWord:  extern proto func (port: UInt16) -> UInt16
 
-outLong: extern(halOutPortLong) proto func (port: UInt16, val: UInt32)
-inLong:  extern(halInPortLong)  proto func (port: UInt16) -> UInt32
+outLong: extern proto func (port: UInt16, val: UInt32)
+inLong:  extern proto func (port: UInt16) -> UInt32
