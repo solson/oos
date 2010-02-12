d242c371111a6acebf6a82af096b86de5ce389eb
Scott Olson <scott@scott-olson.org>
2010-02-11 22:47:27 -0600

    4 spaces instead of tabs.

--- Include/IDT.h
+++ Include/IDT.h
@@ -5,16 +5,16 @@
 
 /* IDT */
 typedef struct IDTD {
-	uint16_t size;
-	uint32_t offset;
+    uint16_t size;
+    uint32_t offset;
 } __attribute__((packed)) IDTD;
 
 typedef struct IDTG {
-	uint16_t offset_1;
-	uint16_t selector;
-	uint8_t zero;
-	uint8_t type_attr;
-	uint16_t offset_2;
+    uint16_t offset_1;
+    uint16_t selector;
+    uint8_t zero;
+    uint8_t type_attr;
+    uint16_t offset_2;
 } __attribute__((packed)) IDTG;
 
 void loadIDT(IDTD);
