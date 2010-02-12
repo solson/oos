8762bbe5dfad9cb3f44024f0139e8ea402e529aa
Scott Olson <scott@scott-olson.org>
2010-02-12 17:18:18 -0600

    Ported Ports.c to ASM, put it in Hal.asm and removed Ports.c

--- Src/Kernel/Hal/Hal.asm
+++ Src/Kernel/Hal/Hal.asm
@@ -58,6 +58,62 @@ halt:
 
 
 ;;;
+;;; Ports
+;;;
+
+;; outByte: func (port: UInt16, val: UInt8)
+global outByte
+outByte:
+	mov eax, [esp+8] ; val
+	mov edx, [esp+4] ; port
+	out dx, al
+	ret
+
+
+;; inByte: func (port: UInt16) -> UInt8
+global inByte
+inByte:
+	mov edx, [esp+4] ; port
+	in al, dx
+	ret
+
+
+;; outWord: func (port: UInt16, val: UInt16)
+global outWord
+outWord:
+	mov eax, [esp+8] ; val
+	mov edx, [esp+4] ; port
+	out dx, ax
+	ret
+
+
+;; inWord: func (port: UInt16) -> UInt16
+global inWord
+inWord:
+	mov edx, [esp+4] ; port
+	in ax, dx
+	ret
+
+
+;; outLong: func (port: UInt16, val: UInt32)
+global outLong
+outLong:
+	mov eax, [esp+8] ; val
+	mov edx, [esp+4] ; port
+	out dx, eax
+	ret
+
+
+;; inLong: func (port: UInt16) -> UInt32
+global inLong
+inLong:
+	mov edx, [esp+4] ; port
+	in eax, dx
+	ret
+
+
+
+;;;
 ;;; Exceptions, ISRs, IRQs, SysCalls
 ;;;
 
