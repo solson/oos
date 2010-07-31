d6b8ee7902e7b20e68f5e6cc03e6905d5ee08339
Scott Olson <scott@scott-olson.org>
2010-07-30 19:32:40 -0600

    Some ASM paging functions we'll need.

--- Src/Kernel/Hal/Hal.asm
+++ Src/Kernel/Hal/Hal.asm
@@ -63,6 +63,29 @@ stackDump:
    pop   ebp
    ret
 
+
+;;;
+;;; Paging
+;;;
+
+;;; Stolen from OSDynamix:
+;;; #define INVLPG(virt)             ASMV("invlpg (%%eax)" : : "a" (virt))
+;;; #define SWITCH_ADDRESS_SPACE(x)  ASMV("mov %0, %%CR3" : : "r" ((dword) (x)))
+
+;;; invalidatePage: func (virt: Pointer)
+global invalidatePage
+invalidatePage:
+   mov   eax, [esp+4]           ; virt
+   invlpg [eax]                 
+   ret
+
+;;; switchAddressSpace: func (addressSpace: UInt32)
+global switchAddressSpace
+switchAddressSpace:
+   ;; FIXME: Can I just mov cr3, [esp+4]?
+   mov   eax, [esp+4]           ; addressSpace
+   mov   cr3, eax
+   ret
    
 ;;;
 ;;; Ports
